605d43988c9c884385b7b5aa8f8bb1a49fe6079a
Chris Fuentes <cfuentes@g.harvard.edu>
2018-03-13 16:19:52 +0100

    #first

---
+++ twas-brillig/twas-brillig/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  twas-brillig
+//
+//  Created by Chris Fuentes on 3/13/18.
+//  Copyright © 2018 microsoft. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
